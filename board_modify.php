cd98e32c02fd6d93f953f97366ca484e78ba766d
TunaF <tmdgns0903@naver.com>
2021-01-01 19:56:19 +0900

    backup

--- board_modify.php
+++ board_modify.php
@@ -11,28 +11,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="./css/board_modify.css">
     <title>think</title>
 </head>
 <body>
     <header>
-        <div><!--Think!-->
-            <a href="./"><img src="#" width="#" height="#" alt="Think"></a>
-        </div>
-        <div> <!--상단 메뉴 페이지-->
+        <!--Think!-->
+        <a href="./index.php">Think</a>
+        
+        <div id="top_list"> <!--상단 메뉴 페이지-->
             <ul>
                 <li><a href="./">HOME</a></li>
                 <li><a href=""><?php
                     if(!empty($_SESSION['id'])){
-                        echo "<a href=\"./logout.php\">로그아웃</a>";
+                        echo "<a href=\"./logout.php\">logout</a>";
                     }else{
-                        echo "<a href=\"./login.php\">로그인</a>";
+                        echo "<a href=\"./login.php\">login</a>";
                     }
                 ?></a></li>
-                <li><a href="./join.php">회원가입</a></li>
+                <li><a href="./join.php">join</a></li>
             </ul>
         </div>
     </header>
+
     <article><!--게시글-->
+        <h1>Modify Page</h1>
+        <div>
+           <hr>
+        </div>
+
         <?php
             $sql = "SELECT * FROM think_board WHERE id={$_GET['idx']}";
 
@@ -44,26 +51,36 @@
             }
             
         ?>
+
         <div>
-            <span>author:<?=$row['autor'];?></span><span>Date:<?=$row['created'];?></span><hr>
             <form action="action.php" enctype="multipart/form-data" method="POST">
                 <input type="hidden" name="mode" value="modify">
                 <input type="hidden" name="id" value="<?=$_GET['idx']?>">
-                <div><span>Title:</span><input type="text" name="title" value="<?=$row['title']?>"></div><hr>
-                <div><span>contents:</span><textarea name="description"><?=$row['description']?></textarea></div><hr>
-                <div><span>File:</span>
-                <?if(!empty($row["file_real"])){ $file_name=iconv('EUC-KR','UTF-8',$row["file_real"])?>
-                    <span>[이미 업로드한 파일]<?=$file_name;?></span><hr>
-                <?}?>
-                <div><input type="file" name="userfile"></div><hr>
-                <input type="submit" value="수정하기">
+
+                <div class="title">
+                    <span>제목</span>
+                    <input type="text" name="title" value="<?=$row['title']?>" required>
+                </div>
+
+                <div class="contents">
+                    <div>내용</div>
+                    <textarea name="description" required><?=$row['description']?></textarea>
+                </div>
+
+                <div class="sub_menu">
+                    <?if(!empty($row["file_real"])){ $file_name=iconv('EUC-KR','UTF-8',$row["file_real"])?>
+                        <hr><span>[이미 업로드한 파일]<?=$file_name;?></span><hr>
+                    <?}?>
+                    <div>File</div>
+                    <div class="file"><input type="file" name="userfile"></div><hr>
+                    
+                    <div class="submit">
+                        <input type="submit" value="수정하기">
+                    </div>
+                </div>                
             </form>
-            <button onclick="location.href='./board.php'">뒤로</button>
+            <button onclick="location.href='./board.php'">뒤로가기</button>
         </div>
     </article>
-    <footer><!--비즈니스문의-->
-        <div>
-        </div>
-    </footer>
 </body>
 </html>
